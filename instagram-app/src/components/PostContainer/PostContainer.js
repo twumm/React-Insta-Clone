d7f6b2382af64de3b46c986943b33586f6dd8117
Martin Twum Mensah <martint.mensah@gmail.com>
2019-06-03 13:25:19 +0000

    feature: Complete PostContainer component Pass required props

---
+++ instagram-app/src/components/PostContainer/PostContainer.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import Post from './Post'
+import CommentSection from '../CommentSection/CommentSection'
+
+const PostContainer = ({ post }) => {
+  return (
+    <div>
+      <Post
+        thumbnailUrl={post.thumbnailUrl}
+        username={post.username}
+        postImageUrl={post.imageUrl}
+      />
+      <CommentSection
+        comments={post.comments}
+      />
+    </div>
+  )
+}
+
+export default PostContainer
