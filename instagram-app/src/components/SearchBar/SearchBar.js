b439ec365abbcf4b04935fdc843d19b5d633f614
Martin Twum Mensah <martint.mensah@gmail.com>
2019-06-03 12:06:56 +0000

    feature: Complete basic seach bar without style but all assets

---
+++ instagram-app/src/components/SearchBar/SearchBar.js
@@ -0,0 +1,37 @@
+import React from 'react'
+import instagramTextLogo from '../../assets/img/instagram-text-logo.png'
+import instagramLogo from '../../assets/img/instagram.svg'
+import compass from '../../assets/img/compass.svg'
+import like from '../../assets/img/heart.svg'
+import profile from '../../assets/img/profile.svg'
+
+const SearchBar = ({ onSearchInputChange }) => {
+  return (
+    <div className="search-bar">
+      <div className="logo-section">
+        <div>
+          <img src={instagramLogo} width={50} alt="Instagram" />
+        </div>
+        <div>
+          <img src={instagramTextLogo} width={150} alt="Instagram" />
+        </div>
+      </div>
+
+      <div className="search-section">
+        <input
+          type="text"
+          placeholder="Search"
+          onChange={onSearchInputChange}
+        />
+      </div>
+
+      <div className="user-activity-section">
+        <img src={compass} width={30} alt="Location" />
+        <img src={like} width={30} alt="Like" />
+        <img src={profile} width={30} alt="Profile" />
+      </div>
+    </div>
+  )
+}
+
+export default SearchBar
