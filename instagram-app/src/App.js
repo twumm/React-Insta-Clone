2ac4ef372e12d2b272ede83114d7cfd35c8c201c
Martin Twum Mensah <martint.mensah@gmail.com>
2019-06-03 13:12:09 +0000

    feature: Import and add PostContainer

--- instagram-app/src/App.js
+++ instagram-app/src/App.js
@@ -1,11 +1,21 @@
 import React from 'react';
 import SearchBar from './components/SearchBar/SearchBar'
+import PostContainer from './components/PostContainer/PostContainer'
+
+import dummyData from './dummy-data'
 import './App.css';
 
 function App() {
   return (
     <div className="">
       <SearchBar />
+      {
+        dummyData
+          .map(post => (
+            <PostContainer key={post.id} post={post} />
+          ))
+      }
+      
     </div>
   );
 }
