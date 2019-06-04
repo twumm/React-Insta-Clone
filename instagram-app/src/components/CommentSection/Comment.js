d9e33bfd936c161d249c3151d6bf2f7ed6a65c16
Martin Twum Mensah <martint.mensah@gmail.com>
2019-06-04 18:54:04 +0000

    feature: Add delete icon Receive props to delete comment

--- instagram-app/src/components/CommentSection/Comment.js
+++ instagram-app/src/components/CommentSection/Comment.js
@@ -2,16 +2,24 @@ import React from 'react'
 import PropTypes from 'prop-types'
 import './CommentSection.css'
 
-const Comment = ({ comment }) => {
+const Comment = ({ comment, commentIndex, postIndex, deleteCommentHandler }) => {
+  const onDeleteComment = (event, postIndex, commentIndex) => {
+    deleteCommentHandler(event, postIndex, commentIndex)
+  }
+
   return (
     <div className="user-comment">
-      {/* <h6>{comment.username}</h6>
-      <p>{comment.text}</p> */}
       <p className="comment-user">
         {comment.username}
         <span className="comment-text">
           {comment.text}
         </span>
+      </p>
+      <p
+        className="comment-delete"
+        onClick={(event) => onDeleteComment(event, postIndex, commentIndex)}
+      >
+        x
       </p>
     </div>
   )
