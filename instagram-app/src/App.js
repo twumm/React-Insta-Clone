0684788d562ba9e89db9db398e59cdaaef9e726e
Martin Twum Mensah <martint.mensah@gmail.com>
2019-06-04 12:25:19 +0000

    feature: Set comments and posts in state On mounting, set the comments and posts from dummyData.js Create methods to add new comments Pass the methods down as props

--- instagram-app/src/App.js
+++ instagram-app/src/App.js
@@ -1,29 +1,86 @@
-import React, { useState } from 'react';
+import React, { Component } from 'react';
 import SearchBar from './components/SearchBar/SearchBar'
 import PostContainer from './components/PostContainer/PostContainer'
 
 import dummyData from './dummy-data'
 import './App.css';
 
-function App() {
-  const [posts, setPost] = useState(dummyData)
+class App extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      posts: [],
+      comments: [],
+      newComment: ''
+    }
+  }
 
-  return (
-    <div className="app-container">
-      <div className="search-bar-container">
-        <SearchBar />
-      </div>
-      <div className="main-post-container">
-        {
-          posts
-            .map(post => (
-              <PostContainer key={post.timestamp.replace(/\s+/g, '')} post={post} />
-            ))
+  componentDidMount() {
+    this.setState({
+      posts: dummyData,
+      comments: dummyData.map(post => post.comments)
+    })
+  }
+
+  addCommentInputChange = (event) => {
+    // console.log('change')
+    const newComment = event.target.value
+    this.setState({
+      newComment: newComment
+    })
+  }
+
+  addNewComment = (event, postIndex) => {
+    // debugger
+    event.preventDefault();
+    const newComment = {
+      username: "randomUser",
+      text: this.state.newComment,
+    }
+
+    const commentToUpdate = this.state.comments[postIndex];
+    commentToUpdate.push(newComment)
+
+    const updatedComments = this.state.comments
+      .map((comment, index) => {
+        if (index === postIndex) {
+          comment = commentToUpdate
         }
+        return comment
+      })
+
+    this.setState({
+      comments: updatedComments,
+      newComment: ''
+    })
+  }
+
+  render() {
+    return (
+      <div className="app-container">
+        <div className="search-bar-container">
+          <SearchBar />
+        </div>
+        <div className="main-post-container">
+          {
+            this.state.posts
+              .map((post, index) => (
+                <PostContainer
+                  key={post.timestamp.replace(/\s+/g, '')}
+                  post={post}
+                  postIndex={index}
+                  comments={this.state.comments}
+                  newComment={this.state.newComment}
+                  addCommentInputChange={this.addCommentInputChange}
+                  addNewComment={this.addNewComment}
+                />
+              ))
+          }
+        </div>
+        
       </div>
-      
-    </div>
-  );
+    );
+  }
 }
 
 export default App;
