bc2631b4e41d45bbfa7a550fa9842e6d202cc184
Martin Twum Mensah <martint.mensah@gmail.com>
2019-06-05 13:44:32 +0000

    refactor: Add state to check if user is logged in On mounting the component, check logged in status of user If user is logged in, render PostsPage, else LoginPage

--- instagram-app/src/authentication/withAuthenticate.js
+++ instagram-app/src/authentication/withAuthenticate.js
@@ -1,11 +1,37 @@
 import React from 'react'
+import LoginPage from '../components/Login/LoginPage'
 
 const withAuthenticate = App => {
   return class extends React.Component {
+    constructor(props) {
+      super(props)
+      this.state = {
+        loggedIn: false
+      }
+    }
+
+    componentDidMount() {
+      const user = localStorage.getItem('username')
+      if (user) {
+        this.setState({
+          loggedIn: true
+        })
+      } else {
+        this.setState({
+          loggedIn: false
+        })
+      }
+    }
+
     render() {
-      return (
-        <App {...this.props} />
-      )
+
+      if (this.state.loggedIn) {
+        return (
+          <App {...this.props} />
+        )
+      } else {
+        return <LoginPage />
+      }
     }
   }
 }
